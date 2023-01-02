ee93a461edb87327872aa424f12aa6041cd21681
Kiyo5hi <44930252+Kiyo5hi@users.noreply.github.com>
2023-01-02 00:16:52 +0000

    feat(fe): added default layout

---
+++ client/components/AppSidebar.vue
@@ -0,0 +1,18 @@
+<template>
+    <aside class="bg-base-200 w-80">
+        <div
+            class="z-20 bg-base-200 bg-opacity-90 backdrop-blur sticky top-0 items-center gap-2 px-4 py-2 hidden lg:flex">
+            <a class="flex-0 btn btn-ghost px-2">
+                <AppLogo></AppLogo>
+            </a>
+        </div>
+
+        <div class="h-4"></div>
+        <ul class="menu flex flex-col p-0 px-4">
+            <!-- Sidebar content here -->
+            <li><a>Home</a></li>
+            <li><a>Hitokoto</a></li>
+            <li><a>Our Story</a></li>
+        </ul>
+    </aside>
+</template>
---
+++ client/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="drawer drawer-mobile">
+        <input id="sidebar" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content flex flex-col">
+            <!-- Navbar -->
+            <AppNavbar />
+
+            <!-- Page content here -->
+            <div class="p-4">
+                <slot></slot>
+            </div>
+        </div>
+
+        <div class="drawer-side">
+            <label for="sidebar" class="drawer-overlay"></label>
+            <AppSidebar />
+        </div>
+    </div>
+</template>
