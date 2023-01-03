488685b0e92d75addd1e203bf9894b779c86dbc3
Kiyo5hi <44930252+Kiyo5hi@users.noreply.github.com>
2023-01-03 07:50:34 +0000

    feat(fe): added temporary timeline

--- client/components/AppNavbar.vue
+++ client/components/AppNavbar.vue
@@ -1,24 +1,31 @@
 <template>
-  <div class="w-full navbar bg-base-300 sticky top-0 opacity-80">
-    <div class="flex-none lg:hidden">
-      <label for="sidebar" class="btn btn-square btn-ghost">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          class="inline-block w-6 h-6 stroke-current"
-        >
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-        </svg>
-      </label>
-    </div>
-    <div class="flex-1 px-2 mx-2">
-      <div class="lg:hidden">
-        <AppLogo />
+  <div
+    class="
+  sticky top-0 z-30 flex h-16 w-full justify-center bg-opacity-90 backdrop-blur transition-all duration-100
+  bg-base-300 text-base-content shadow-sm
+  "
+  >
+    <nav class="navbar w-full">
+      <div class="flex-none lg:hidden">
+        <label for="sidebar" class="btn btn-square btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            class="inline-block w-6 h-6 stroke-current"
+          >
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </label>
       </div>
-      <div class="ml-2">
-        🐕 🌼 🦆
+      <div class="flex-1 px-2 mx-2">
+        <div class="lg:hidden">
+          <AppLogo />
+        </div>
+        <div class="ml-2">
+          🐕 🌼 🦆
+        </div>
       </div>
-    </div>
+    </nav>
   </div>
 </template>
---
+++ client/pages/our-story.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="flex flex-col items-center justify-center">
+    <ul class="steps steps-vertical">
+      <li v-for="event in events" :key="event.event" data-content="●" class="step step-primary">
+        {{ event.date }}: {{ event.event }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const events = [{
+  date: '2022-06-18',
+  event: '初识'
+}, {
+  date: '2022-06-27',
+  event: '初见'
+}, {
+  date: '2022-07-06',
+  event: 'Kiyoshi 纹身'
+}, {
+  date: '2022-07-11',
+  event: '狗美甲'
+}, {
+  date: '2022-07-19',
+  event: '开始'
+}, {
+  date: '2022-07-26',
+  event: '吵架'
+}, {
+  date: '2022-07-27',
+  event: '纹身'
+}, {
+  date: '2022-08-07',
+  event: 'First Ring'
+}, {
+  date: '2022-09-12',
+  event: '电子化'
+}, {
+  date: '2022-10-06',
+  event: '鸭给狗买的秋装和鸭信 Batch 1 签收'
+}, {
+  date: '2022-10-08',
+  event: '狗信 Batch 1 签收'
+}, {
+  date: '2022-10-22',
+  event: '鸭信 Batch 2 和牵引绳签收'
+}, {
+  date: '2022-10-25',
+  event: '狗信 Batch 2 签收'
+}, {
+  date: '2022-11-18',
+  event: '很重要的电子谈心'
+}, {
+  date: '2022-12-15',
+  event: '鸭信 Batch 3 签收'
+}, {
+  date: '2022-12-15',
+  event: '狗信 Batch 3 签收'
+}, {
+  date: '2022-12-27',
+  event: '鸭给狗的圣诞礼物和生日信签收'
+}, {
+  date: '2023-01-01',
+  event: '新篇章'
+}].sort((a, b) => a.date.localeCompare(b.date))
+</script>
